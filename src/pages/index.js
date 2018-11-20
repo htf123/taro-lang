20698c7504f8f0b067e8144d60a528e54249aa06
langwenqi <497379037@qq.com>
2018-11-20 14:11:04 +0800

    subpackages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 
-import { add, minus, asyncAdd } from '../../store/actions/counter'
+import { add, minus, asyncAdd } from '../store/actions/counter'
 
-import './index.styl'
+import '../pageStyle/index.styl'
 
 
 @connect(({ counter }) => ({
@@ -22,20 +22,24 @@ import './index.styl'
 }))
 class Index extends Component {
 
-    config = {
+  config = {
     navigationBarTitleText: '首页'
   }
 
   componentWillReceiveProps (nextProps) {
     console.log(this.props, nextProps)
   }
-
   componentWillUnmount () { }
 
   componentDidShow () { }
 
   componentDidHide () { }
 
+  toPhoto(){
+    Taro.navigateTo({
+      url: '/photo/photo'
+    })
+  }
   render () {
     return (
       <View className='index'>
@@ -43,7 +47,7 @@ class Index extends Component {
         <Button className='dec_btn' onClick={this.props.dec}>-</Button>
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
         <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, LangWenqi</Text></View>
+        <View onClick={this.toPhoto.bind(this)}><Text>Hello, LangWenqi</Text></View>
       </View>
     )
   }
---
+++ src/photo/photo.js
@@ -0,0 +1,52 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../store/actions/counter'
+
+import '../pageStyle/index.styl'
+
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+class Photo extends Component {
+
+  config = {
+    navigationBarTitleText: 'Photo'
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Button className='add_btn' onClick={this.props.add}>+</Button>
+        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
+        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+        <View><Text>{this.props.counter.num}</Text></View>
+        <View><Text>Hello, LangWenqi</Text></View>
+      </View>
+    )
+  }
+}
+
+export default Photo
